2dff1939ed4b7c4e241b2891ee632b20aa9810f2
Dave De Carlo <dec114@gmail.com>
2015-11-02 15:22:57 -0500

    post to ga

--- lib/truck/truck.rb
+++ lib/truck/truck.rb
@@ -1,5 +1,32 @@
 require 'faraday'
 
 module Truck
+  attr_reader :host, :ep, :v, :t, :tid, :cid, :dp
   
+  def initialize
+    @host = "www.google-analytics.com"
+    @ep   = "/collect"
+    @v    = 1
+    @t    = "pageview"
+    @tid  = "UA-69585210-1"
+    @cid  = "20454dc2-f1b4-4fe0-bead-013ad8895100"
+    @dp   = "/"
+  end
+  
+  def rev
+    conn = Faraday.new(:url => 'http://#{host}') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    
+    ## POST ##
+
+    conn.post "#{ep}", { :v   => "#{v}", 
+                         :t   => "#{t}",
+                         :tid => "#{tid}",
+                         :cid => "#{cid}",
+                         :dp  => "#{dp}"
+    }
+  end
 end
